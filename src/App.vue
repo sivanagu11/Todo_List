014a977b65aa81ff5ad955175e50063f25b3555c
nagamani32 <sivanagu0@gmail.com>
2022-01-13 20:25:49 +0530

    added background image & navbar

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <todo msg = ""/>
+</template>
+
+<script>
+import Todo from './components/Todo.vue'
+
+export default {
+  name: 'App',
+  components: {
+        Todo
+  }
+}
+</script>
+
+<style>
+body{
+
+background-image: linear-gradient( 90deg, #00DBDE 0%, #FC00FF 100%);
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,21 @@
+
+   <template>
+  <div class="topnav">
+    <b-navbar-brand><h1 class="nav">Todo Task</h1> </b-navbar-brand>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  compute: {},
+};
+</script>
+
+
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
